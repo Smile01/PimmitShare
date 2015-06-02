03d5f0b16ee37bc21ae388df6e9599a8595a71e6
AshishNR <ashish.raiyani@gmail.com>
2015-06-02 13:15:57 -0400

    adding files
    adding post label ,
    adding couple more files.

--- models/models.js
+++ models/models.js
@@ -11,7 +11,7 @@ var userSchema = new mongoose.Schema({
 	username: String,
 	password: String, //hash created from password
 	created_at: {type: Date, default: Date.now}
-})
+});
 
 
 mongoose.model('Post', postSchema);
